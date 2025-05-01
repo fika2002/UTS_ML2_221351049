7329595e061472a5ba8f12d761c7c54ab2b47cb7
fika <fikasolihin19@gmail.com>
2025-05-01 18:50:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,16 @@ import numpy as np
 import tensorflow as tf
 import pickle
 
-# ========================
-# Load TFLite Model & Scaler
-# ========================
+
 interpreter = tf.lite.Interpreter(model_path="flight_price_model.tflite")
 interpreter.allocate_tensors()
 
-# Ambil detail tensor
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Load scaler
+
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
-# ========================
-# UI
-# ========================
 st.title("Prediksi Harga Tiket Pesawat INDIA")
 
 airline = st.selectbox("Maskapai", ["AirAsia", "IndiGo", "SpiceJet", "Vistara", "GoAir"])
@@ -30,9 +24,7 @@ duration = st.number_input("Durasi Penerbangan (menit)", min_value=60, max_value
 class_type = st.selectbox("Kelas", ["Business","Economy"])
 days_left = st.slider("Sisa Hari Keberangkatan", 1, 60)
 
-# ========================
-# Manual Encoding
-# ========================
+
 airline_dict = {"AirAsia": 0, "IndiGo": 1, "SpiceJet": 2, "Vistara": 3, "GoAir": 4}
 city_dict = {"Delhi": 0, "Mumbai": 1, "Bangalore": 2, "Kolkata": 3, "Hyderabad": 4}
 time_dict = {"Pagi": 0, "Siang": 1, "Sore": 2, "Malam": 3}
@@ -47,12 +39,10 @@ input_data = np.array([[airline_dict[airline],
                         class_dict[class_type],
                         days_left]])
 
-# Scaling
+
 input_scaled = scaler.transform(input_data).astype(np.float32)
 
-# ========================
-# Prediksi TFLite
-# ========================
+
 if st.button("Prediksi"):
     interpreter.set_tensor(input_details[0]['index'], input_scaled)
     interpreter.invoke()
